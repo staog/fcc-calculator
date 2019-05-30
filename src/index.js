0247137cae192f41ddec7c96db705463688630f8
Milan V. Kecojević <staog@gmx.com>
2019-05-30 23:38:17 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,88 +2,181 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const buttons = [
+  {
+    id:'delete',
+    className: 'box gray',
+    name: 'DEL'
+  },
+  {
+    id:'grand',
+    className: 'box gray',
+    name: 'GT'
+  },
+  {
+    id:'clear',
+    className: 'box gray ac',
+    name: 'AC'
+  },
+  {
+    id:'divide',
+    className: 'box gray',
+    name: '/'
+  },
+  {
+    id:'seven',
+    className: 'box',
+    name: '7',
+  },
+  {
+    id:'eight',
+    className: 'box',
+    name: '8'
+  },
+  {
+    id:'nine',
+    className: 'box',
+    name: '9'
+  },
+  {
+    id:'multiply',
+    className: 'box gray',
+    name: '*'
+  },
+  {
+    id:'four',
+    className: 'box',
+    name: '4'
+  },
+  {
+    id:'five',
+    className: 'box',
+    name: '5'
+  },
+  {
+    id:'six',
+    className: 'box',
+    name: '6'
+  },
+  {
+    id:'subtract',
+    className: 'box gray',
+    name: '-'
+  },
+  {
+    id:'one',
+    className: 'box',
+    name: '1'
+  },
+  {
+    id:'two',
+    className: 'box',
+    name: '2'
+  },
+  {
+    id:'three',
+    className: 'box',
+    name: '3'
+  },
+  {
+    id:'add',
+    className: 'box gray',
+    name: '+'
+  },
+  {
+    id:'zero',
+    className: 'box',
+    name: '0',
+  },
+  {
+    id:'decimal',
+    className: 'box',
+    name: '.'
+  },
+  {
+    id:'equals',
+    className: 'wide blue',
+    name: '='
+  },
+
+]
+
 class Calculator extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      currentNumber: "0"
+      current: "0",
+      operator: false,
+      decimal: false
     }
   }
 
-  handleClick = (buttonName) => {
-    let currentNumber = this.state.currentNumber
-    console.log(buttonName);
+  handleClick = (e) => {
+    let current = this.state.current
+    let operator = this.state.operator
     switch (true) {
-      case buttonName === "0" ||
-           buttonName === "1" ||
-           buttonName === "2" ||
-           buttonName === "3" ||
-           buttonName === "4" ||
-           buttonName === "5" ||
-           buttonName === "6" ||
-           buttonName === "7" ||
-           buttonName === "8" ||
-           buttonName === "9" :
-      if (this.state.currentNumber !== "0") {
-        currentNumber += buttonName
-      }
-      else {
-        currentNumber = buttonName
-      }
-      this.setState({currentNumber})
-      break
+      case /[/*\-+]/.test(e.target.name):
+        if (!this.state.operator) {
+          current += e.target.name
+          operator = true
+          this.setState({decimal: false})
+        }
+        else {
+          const modified = current.slice(0, current.length - 1)
+          current = modified
+          current += e.target.name
+        }
+        break
+      case /\d/.test(e.target.name):
+        if (this.state.current !== "0") {
+          current += e.target.name
+        }
+        else {
+          current = e.target.name
+        }
+        operator = false
+        break
+      case /\bclear\b/.test(e.target.id):
+        current = "0"
+        operator = false
+        this.setState({decimal: false})
+        break
+      case /[=]/.test(e.target.name):
+        current = eval(current)
+        break
+      case /\./.test(e.target.name):
+        if (!this.state.decimal) {
+          current += "."
+          this.setState({decimal: true})
+        }
+        break
+      case /\bdelete\b/.test(e.target.id):
+        if (current.length > 1) {
+          current = current.substring(0, current.length - 1)
+        }
+        else {
+          current = "0"
+        }
     }
+    this.setState({
+      current: current,
+      operator: operator
+    })
   }
 
   render() {
-    return(
+    return (
       <div id="calculator">
         <div id="topDisplay"><span>CASIO</span>MJ-120D Plus</div>
-        <Display currentNumber={this.state.currentNumber} />
+        <div id="display">
+          <p>{this.state.current}</p>
+        </div>
         <div id="container">
-          <Button id="par1" name="(" handleClick={this.handleClick} />
-          <Button id="par2" name=")" handleClick={this.handleClick} />
-          <Button id="clear" name="AC" handleClick={this.handleClick} />
-          <Button id="clear" name="C" handleClick={this.handleClick} />
-          <Button id="seven" name="7" handleClick={this.handleClick} />
-          <Button id="eight" name="8" handleClick={this.handleClick} />
-          <Button id="nine" name="9" handleClick={this.handleClick} />
-          <Button id="Buttonide" name="/" handleClick={this.handleClick} />
-          <Button id="four" name="4" handleClick={this.handleClick} />
-          <Button id="five" name="5" handleClick={this.handleClick} />
-          <Button id="six" name="6" handleClick={this.handleClick} />
-          <Button id="multiply" name="*" handleClick={this.handleClick} />
-          <Button id="one" name="1" handleClick={this.handleClick} />
-          <Button id="two" name="2" handleClick={this.handleClick} />
-          <Button id="three" name="3" handleClick={this.handleClick} />
-          <Button id="subtract" name="-" handleClick={this.handleClick} />
-          <Button id="zero" name="0" handleClick={this.handleClick} />
-          <Button id="decimal" name="." handleClick={this.handleClick} />
-          <Button id="equals" name="=" handleClick={this.handleClick} />
-          <Button id="add" name="+" handleClick={this.handleClick} />
+          {buttons.map((item, i) => {
+            return <button id={item.id} className={item.className} key={i} name={item.name} onClick={this.handleClick}>{item.name}</button>
+          })}
         </div>
       </div>
-    )
-  }
-}
-
-class Display extends React.Component {
-  render() {
-    return(
-      <div id="display">
-        {this.props.currentNumber}
-      </div>
-    )
-  }
-}
-
-class Button extends React.Component {
-  runHandleClick = () => {
-    this.props.handleClick(this.props.name)
-  }
-  render() {
-    return(
-      <div id={this.props.id} className="box click" onClick={this.props.runHandleClick}>{this.props.name}</div>
     )
   }
 }
